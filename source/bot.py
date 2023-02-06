e29c3ec9a66beb2cbe81c6442e9dd19cf3d57134
Christian U <christian@ullihome.de>
2023-02-06 23:52:43 +0100

    more analyse stuff

--- source/bot.py
+++ source/bot.py
@@ -7,151 +7,158 @@ class Portfolio(Config):
         super().__init__(room, **kwargs)
 @bot.listener.on_message_event
 async def tell(room, message):
-    global servers,lastsend
-    logging.info(str(message))
-    if not message.body.startswith(prefix) and room.member_count==2:
-        message.body = prefix+' '+message.body
-    match = botlib.MessageMatch(room, message, bot, prefix)
-    if (match.is_not_from_this_bot() and match.prefix())\
-    and (match.command("buy")\
-      or match.command("sell")\
-      or match.command("add")):
-        depot = None
-        count = None
-        if len(match.args())>2:
-            count = float(match.args()[2])
-        price = None
-        if len(match.args())>4:
-            depot = float(match.args()[4])
-        if len(match.args())>3:
-            price = float(match.args()[3])
-        for adepot in servers:
-            if adepot.room == room.room_id and (adepot.name == depot or depot == None):
-                depot = adepot
-        if not depot is str:
-            npaper = None
-            found = False
-            for paper in depot.papers:
-                if paper['isin'] == match.args()[1]:
-                    found = True
-                    if not price:
-                        price = database.GetActPrice(paper)
-                    if not count:
-                        count = paper['count']
-                    break
-            if not found:
-                paper ={
-                    'isin': match.args()[1],
-                    'count': 0,
-                    'price': 0
-                }
-                depot.papers.append(paper)
-            if 'lastcount' in paper and count == None:
-                count = paper['lastcount']
-            if paper['count'] > 0:
-                paper['lastcount'] = paper['count']
-            for paper in depot.papers:
-                if paper['isin'] == match.args()[1]:
-                    oldprice = float(paper['price'])
-                    if not count:
-                        count = 0
-                    if not price:
-                        price = oldprice
-                    newprice = price*count
-                    if match.command("buy"):
-                        paper['price'] = oldprice+newprice
-                        paper['count'] = paper['count']+count
-                    elif match.command("sell"):
-                        if newprice>oldprice:
-                            newprice = oldprice
-                        paper['price'] = oldprice-newprice
-                        paper['count'] = paper['count']-count
-                    await save_servers()
-                    await bot.api.send_text_message(room.room_id, 'ok')
-                    loop = asyncio.get_running_loop()
-                    loop.create_task(check_depot(depot,True))
-                    break
-    elif (match.is_not_from_this_bot() and match.prefix())\
-    and match.command("analyze"):
-        depot = None
-        strategy = 'sma'
-        if len(match.args())>2: strategy = match.args()[2]
-        days = 1
-        if len(match.args())>3: days = float(match.args()[3])
-        date = None
-        if len(match.args())>4: date = match.args()[4]
-        for adepot in servers:
-            if adepot.room == room.room_id and (adepot.name == depot or depot == None):
-                depot = adepot
-        if not depot is str:
-            npaper = None
-            found = False
-            for paper in depot.papers:
-                if paper['isin'] == match.args()[1]:
-                    df = await database.GetPaperData(paper,days)
-                    vola = 0.0
-                    for index, row in df.iterrows():
-                        avola = ((row['Max']-row['Min'])/row['Close'])*100
-                        if avola > vola: vola = avola
-                    msg =  'Analyse of %s (%s)\n' % (paper['name'],paper['isin'])\
-                          +'Volatility: %.2f\n' % vola
-                    await bot.api.send_markdown_message(room.room_id, msg)
-    elif (match.is_not_from_this_bot() and match.prefix())\
-    and match.command("show"):
-        tdepot = None
-        msg = ''
-        if len(match.args())>1:
-            tdepot = match.args()[1]
-        for depot in servers:
-            if depot.room == room.room_id and (depot.name == tdepot or tdepot == None):
-                msg += '<h3>%s</h3>' % depot.name
-                msg += '<table style="text-align: right">\n'
-                msg += '<tr><th>Paper</th><th>Name</th><th>Price</th><th>Change</th></tr>\n'
-                sumprice = 0
-                sumactprice = 0
-                sellcosts = 0
+    try:
+        global servers,lastsend
+        logging.info(str(message))
+        if not message.body.startswith(prefix) and room.member_count==2:
+            message.body = prefix+' '+message.body
+        match = botlib.MessageMatch(room, message, bot, prefix)
+        if (match.is_not_from_this_bot() and match.prefix())\
+        and (match.command("buy")\
+        or match.command("sell")\
+        or match.command("add")):
+            depot = None
+            count = None
+            if len(match.args())>2:
+                count = float(match.args()[2])
+            price = None
+            if len(match.args())>4:
+                depot = float(match.args()[4])
+            if len(match.args())>3:
+                price = float(match.args()[3])
+            for adepot in servers:
+                if adepot.room == room.room_id and (adepot.name == depot or depot == None):
+                    depot = adepot
+            if not depot is str:
+                npaper = None
+                found = False
                 for paper in depot.papers:
-                    if 'name' in paper and paper['count'] > 0:
-                        actprice = database.GetActPrice(paper)
-                        sumactprice += actprice*paper['count']
-                        sumprice += paper['price']
-                        change = (actprice*paper['count'])-paper['price']
-                        sellcosts += ((sumactprice-sumprice)*(depot.tradingCostPercent/100))+depot.tradingCost
-                        msg += '<tr><td>'+paper['isin']+'</td><td>%.0fx' % paper['count']+paper['name']+'</td><td align=right>%.2f (%.2f)' % ((actprice*paper['count']),actprice)+'</td><td align=right>%.2f' % change+'</td></tr>\n'
-                msg += '<tr><td></td><td></td><td align=right>%.2f' % sumactprice+'</td><td align=right>%.2f' % (sumactprice-sumprice)+'</td></tr>\n'
-                msg += '<tr><td></td><td></td><td>Taxes</td><td align=right>%.2f' % -((sumactprice-sumprice)*(depot.taxCostPercent/100))+'</td></tr>\n'
-                msg += '<tr><td></td><td></td><td>Sell-Costs</td><td align=right>%.2f' % -(sellcosts)+'</td></tr>\n'
-                msg += '<tr><td></td><td></td><td>Complete</td><td>%.2f' % ((sumactprice-sumprice)-(((sumactprice-sumprice)*(depot.taxCostPercent/100))+sellcosts))+'</td></tr>\n'
-                msg += '</table>\n'
-        await bot.api.send_markdown_message(room.room_id, msg)
-    elif (match.is_not_from_this_bot() and match.prefix())\
-    and match.command("create-depot"):
-        pf = None
-        for server in servers:
-            if server.room == room.room_id and server.name == match.args()[1]:
-                pf = server
-        if not pf:
-            pf = Portfolio({
-                'room': room.room_id,
-                'name': match.args()[1],
-                'taxCostPercent': 0.0,
-                'tradingCost': 0.0,
-                'tradingCostPercent': 0.0,
-                'currency': 'EUR',
-                'papers': []
-            })
-        if len(match.args())>5:
-            pf.currency = match.args()[5]
-        if len(match.args())>4:
-            pf.tradingCostPercent = float(match.args()[4])
-        if len(match.args())>3:
-            pf.tradingCost = float(match.args()[3])
-        if len(match.args())>2:
-            pf.taxCostPercent = float(match.args()[2])
-        servers.append(pf)
-        loop.create_task(check_depot(pf))
-        await save_servers()
-        await bot.api.send_text_message(room.room_id, 'ok')
+                    if paper['isin'] == match.args()[1]:
+                        found = True
+                        if not price:
+                            price = database.GetActPrice(paper)
+                        if not count:
+                            count = paper['count']
+                        break
+                if not found:
+                    paper ={
+                        'isin': match.args()[1],
+                        'count': 0,
+                        'price': 0
+                    }
+                    depot.papers.append(paper)
+                if 'lastcount' in paper and count == None:
+                    count = paper['lastcount']
+                if paper['count'] > 0:
+                    paper['lastcount'] = paper['count']
+                for paper in depot.papers:
+                    if paper['isin'] == match.args()[1]:
+                        oldprice = float(paper['price'])
+                        if not count:
+                            count = 0
+                        if not price:
+                            price = oldprice
+                        newprice = price*count
+                        if match.command("buy"):
+                            paper['price'] = oldprice+newprice
+                            paper['count'] = paper['count']+count
+                        elif match.command("sell"):
+                            if newprice>oldprice:
+                                newprice = oldprice
+                            paper['price'] = oldprice-newprice
+                            paper['count'] = paper['count']-count
+                        await save_servers()
+                        await bot.api.send_text_message(room.room_id, 'ok')
+                        loop = asyncio.get_running_loop()
+                        loop.create_task(check_depot(depot,True))
+                        break
+        elif (match.is_not_from_this_bot() and match.prefix())\
+        and match.command("analyze"):
+            depot = None
+            strategy = 'sma'
+            if len(match.args())>2: strategy = match.args()[2]
+            days = 1
+            if len(match.args())>3: days = float(match.args()[3])
+            date = None
+            if len(match.args())>4: date = match.args()[4]
+            for adepot in servers:
+                if adepot.room == room.room_id and (adepot.name == depot or depot == None):
+                    depot = adepot
+            if not depot is str:
+                npaper = None
+                found = False
+                for paper in depot.papers:
+                    if paper['isin'] == match.args()[1]:
+                        df = await database.GetPaperData(paper,days+1)
+                        vola = 0.0
+                        for index, row in df.iterrows():
+                            avola = ((row['High']-row['Low'])/row['Close'])*100
+                            if avola > vola: vola = avola
+                        msg = 'Analyse of %s (%s)\n' % (paper['name'],paper['isin'])\
+                             +'Change: %.2f\n' % (float(df.iloc[-1]['Close'])-float(df.iloc[0]['Close']))\
+                             +'Volatility: %.2f\n' % vola
+                        await bot.api.send_markdown_message(room.room_id, msg)
+        elif (match.is_not_from_this_bot() and match.prefix())\
+        and match.command("show"):
+            tdepot = None
+            msg = ''
+            if len(match.args())>1:
+                tdepot = match.args()[1]
+            for depot in servers:
+                if depot.room == room.room_id and (depot.name == tdepot or tdepot == None):
+                    msg += '<h3>%s</h3>' % depot.name
+                    msg += '<table style="text-align: right">\n'
+                    msg += '<tr><th>Paper</th><th>Name</th><th>Price</th><th>Change</th></tr>\n'
+                    sumprice = 0
+                    sumactprice = 0
+                    sellcosts = 0
+                    for paper in depot.papers:
+                        if 'name' in paper and paper['count'] > 0:
+                            actprice = database.GetActPrice(paper)
+                            sumactprice += actprice*paper['count']
+                            sumprice += paper['price']
+                            change = (actprice*paper['count'])-paper['price']
+                            sellcosts += ((sumactprice-sumprice)*(depot.tradingCostPercent/100))+depot.tradingCost
+                            msg += '<tr><td>'+paper['isin']+'</td><td>%.0fx' % paper['count']+paper['name']+'</td><td align=right>%.2f (%.2f)' % ((actprice*paper['count']),actprice)+'</td><td align=right>%.2f' % change+'</td></tr>\n'
+                    msg += '<tr><td></td><td></td><td align=right>%.2f' % sumactprice+'</td><td align=right>%.2f' % (sumactprice-sumprice)+'</td></tr>\n'
+                    msg += '<tr><td></td><td></td><td>Taxes</td><td align=right>%.2f' % -((sumactprice-sumprice)*(depot.taxCostPercent/100))+'</td></tr>\n'
+                    msg += '<tr><td></td><td></td><td>Sell-Costs</td><td align=right>%.2f' % -(sellcosts)+'</td></tr>\n'
+                    msg += '<tr><td></td><td></td><td>Complete</td><td>%.2f' % ((sumactprice-sumprice)-(((sumactprice-sumprice)*(depot.taxCostPercent/100))+sellcosts))+'</td></tr>\n'
+                    msg += '</table>\n'
+            await bot.api.send_markdown_message(room.room_id, msg)
+        elif (match.is_not_from_this_bot() and match.prefix())\
+        and match.command("create-depot"):
+            pf = None
+            for server in servers:
+                if server.room == room.room_id and server.name == match.args()[1]:
+                    pf = server
+            if not pf:
+                pf = Portfolio({
+                    'room': room.room_id,
+                    'name': match.args()[1],
+                    'taxCostPercent': 0.0,
+                    'tradingCost': 0.0,
+                    'tradingCostPercent': 0.0,
+                    'currency': 'EUR',
+                    'papers': []
+                })
+            if len(match.args())>5:
+                pf.currency = match.args()[5]
+            if len(match.args())>4:
+                pf.tradingCostPercent = float(match.args()[4])
+            if len(match.args())>3:
+                pf.tradingCost = float(match.args()[3])
+            if len(match.args())>2:
+                pf.taxCostPercent = float(match.args()[2])
+            servers.append(pf)
+            loop.create_task(check_depot(pf))
+            await save_servers()
+            await bot.api.send_text_message(room.room_id, 'ok')
+    except BaseException as e:
+        logging.error(str(e), exc_info=True)
+        if not hasattr(depot,'lasterror') or depot.lasterror != str(e):
+            await bot.api.send_text_message(depot.room,str(depot.name)+': '+str(e))
+            depot.lasterror = str(e)
 paper_strategies = []
 async def ProcessStrategy(paper,depot,data):
     paper_strategy = None
