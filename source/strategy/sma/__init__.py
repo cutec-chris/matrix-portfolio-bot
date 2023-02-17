5b088d47d69ecd2b6588dc109c5c34dcfa477e3f
Christian U <christian@ullihome.de>
2023-02-17 23:26:54 +0100

    sma tests works

--- source/strategy/sma/__init__.py
+++ source/strategy/sma/__init__.py
@@ -4,7 +4,6 @@ class Strategy(backtrader.Strategy):
         ('fast_sma_period', 12),
         ('slow_sma_period', 26),
     )
-
     def __init__(self):
         self.fast_sma = backtrader.indicators.SimpleMovingAverage(
             self.data.close, period=self.params.fast_sma_period
@@ -18,3 +17,14 @@ class Strategy(backtrader.Strategy):
             self.buy()
         elif self.data.close[0] < self.slow_sma[0] and self.data.close[-1] >= self.slow_sma[-1]:
             self.sell()
+if __name__ == "__main__":
+    import pathlib,sys;sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
+    import database,datetime
+    cerebro = backtrader.Cerebro()
+    cerebro.addstrategy(Strategy)
+    cerebro.broker.setcash(1000)
+    sym = database.session.query(database.Symbol).filter_by(ticker='TSLA').first()
+    data = sym.GetData(datetime.datetime.utcnow()-datetime.timedelta(days=30*3))
+    cerebro.adddata(backtrader.feeds.PandasData(dataname=data))
+    cerebro.run()
+    cerebro.plot()
