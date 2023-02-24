f86d95673561e2ef523e70bb8060c95ade73923e
Christian U <christian@ullihome.de>
2023-02-24 23:12:28 +0100

    use sizer

--- source/strategy/bb/__init__.py
+++ source/strategy/bb/__init__.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     cerebro.addstrategy(Strategy)
     cerebro.broker.setcash(1000)
     cerebro.addsizer(backtrader.sizers.PercentSizer, percents=100)
-    sym = database.session.query(database.Symbol).filter_by(ticker='TSLA').first()
+    sym = database.session.query(database.Symbol).filter_by(ticker='ASML.AS').first()
     data = sym.GetData(datetime.datetime.utcnow()-datetime.timedelta(days=30*3))
     cerebro.adddata(backtrader.feeds.PandasData(dataname=data))
     cerebro.run()
