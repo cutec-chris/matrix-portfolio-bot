47de275a99593206e0ac05a5d7104a3ef744d6df
Christian U <christian@ullihome.de>
2023-02-26 11:19:04 +0100

    remove yfinance dependency

--- source/datasources/yahoo/__init__.py
+++ source/datasources/yahoo/__init__.py
@@ -1,5 +1,5 @@
 import asyncio,aiohttp,csv,datetime,pytz,time
-import requests,yfinance,pandas,pathlib,database,sqlalchemy.sql.expression,asyncio,logging,io
+import requests,pandas,pathlib,database,sqlalchemy.sql.expression,asyncio,logging,io
 UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
 async def UpdateTicker(paper):
     def extract_trading_times(metadata):
