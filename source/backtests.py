901cd54a212d8b9b06377e77e09dbfd45e3cf56e
Christian U <christian@ullihome.de>
2023-04-23 23:04:57 +0200

    standard strategy samples with new structure

--- source/backtests.py
+++ source/backtests.py
@@ -8,13 +8,14 @@ async def default_backtest(Strategy,ticker=None,isin=None,start=datetime.datetim
         sym = await database.FindSymbol(session,{'ticker': ticker,'isin': isin},None)
         if sym:
             data = await sym.GetData(session,start,end)
-            if not data.empty:
-                return None
+            if data.empty:
+                return None,None
         else: return None
     cerebro = backtrader.Cerebro(stdstats=False,cheat_on_open=True)
+    cerebro.adddata(backtrader.feeds.PandasData(dataname=data))
     cerebro.broker.setcash(1000)
     cerebro.addsizer(backtrader.sizers.PercentSizer, percents=33.3)
     cerebro.addobserver(backtrader.observers.BuySell,barplot=True,bardist=0.001)  # buy / sell arrows
     cerebro.addobserver(backtrader.observers.Broker)
     cerebro.addobserver(backtrader.observers.Trades)
-    return await run_backtest(cerebro)
+    return await run_backtest(cerebro),cerebro
