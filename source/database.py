e5f1331bf7a89561d2e4708a8e5ebd59a3207948
Christian U <christian@ullihome.de>
2023-05-22 22:38:42 +0200

    trial to avoid locking more

--- source/database.py
+++ source/database.py
@@ -396,7 +396,7 @@ class BotCerebro(backtrader.Cerebro):
 Data=pathlib.Path('.') / 'data' / 'database.db'
 Data.parent.mkdir(parents=True,exist_ok=True)
 engine=sqlalchemy.ext.asyncio.create_async_engine('sqlite+aiosqlite:///'+str(Data), connect_args={
-        'timeout': 0.5,
+        'timeout': 5,
         'check_same_thread': False,
         'isolation_level': None,
         }) 
